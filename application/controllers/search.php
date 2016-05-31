e5ec96f58047297bc1258bffdd90b84423be471b
Đào Bá Đạt <daobadat7913@outlook.com>
2016-05-31 22:00:07 +0700

    - Cập nhập lại chức năng phân trang - Thêm chức năng Search - Truyền dữ liệu qua detail

---
+++ application/controllers/search.php
@@ -0,0 +1,35 @@
+<?php
+class Search extends Controller
+{
+    private $searchs;
+    private $search;
+    private $listBook;
+    private $currentPage = 1;
+    private $totalOfPage;
+    private $position = 0;
+    function __construct()
+    {
+        //Khai báo model category
+        include("application/models/searchs_model.php");
+
+        $this->searchs = new Searchs();
+        $this->search =  empty($_GET["search"])? "1":$_GET["search"];
+        $this->currentPage = empty($_GET["p"])? "1":$_GET["p"];
+    }
+    function Index()
+    {
+        if($this->currentPage > 1)
+        {
+            $this->position = $this->currentPage * 8 - 8;
+        }
+
+        $this->listBook = $this->searchs->GetBookByName($this->position,8,$this->search);
+
+        $this->totalOfPage = $this->searchs->TotalOfPage($this->search);
+
+        $data = array("listCategory"=>$this->listBook,"totalOfPage" =>$this->totalOfPage, "currentPage" => $this->currentPage, "search" => $this->search);
+        $view = array("Index" => "Index","componentPagination"=>"pagination");
+        $this->View($view,$data);
+    }
+}
+?>
--- application/models/detail_model.php
+++ application/models/detail_model.php
@@ -1,6 +1,16 @@
 <?php
-    class Detail extends Model
+    class Details extends Model
     {
+        function GetBookDetailByID($bookID)
+        {
+            //Câu Select
+            $sql = "SELECT * FROM books WHERE BOOK_ID = $bookID ";
 
+            //Thực thi câu lệnh
+            $runSql = $this->db->QueryResult($sql);
+
+            //Trả về dữ liệu
+            return $runSql->fetchAll(PDO::FETCH_CLASS);
+        }
     }
 ?>
