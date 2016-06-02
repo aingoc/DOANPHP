f954568d7663348ad0683058ee5d52273593e238
Đào Bá Đạt <daobadat7913@outlook.com>
2016-06-02 17:24:58 +0700

    Update trang detail

--- application/controllers/detail.php
+++ application/controllers/detail.php
@@ -4,21 +4,32 @@
         private $details;
         private $bookID;
         private $bookDetail;
+        private $bookSameType;
+        private $books;
+        private $hot;
 
 
         function __construct()
         {
             //Khai báo model category
             include("application/models/detail_model.php");
+            include("application/models/books_model.php");
 
+            $this->books = new Books();
             $this->details = new Details();
             $this->bookID = empty($_GET["id"])? "1":$_GET["id"];
         }
 
         function Index()
         {
+
+            $this->hot = $this->books->SelectHotBooks(0,3);
+
             $this->bookDetail = $this->details->GetBookDetailByID($this->bookID);
-            $data = array("bookDetail"=>$this->bookDetail);
+
+            $this->bookSameType = $this->details->GetBookSameType($this->bookDetail[0]->CATEGORY_ID,0,3);
+
+            $data = array("bookDetail"=>$this->bookDetail,"bookSameType" => $this->bookSameType, "hot"=> $this->hot);
 
             $view = array("Index" => "Index");
             $this->View($view,$data);
