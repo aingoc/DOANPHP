6f4f22b206b5404880c4a3f0ceb5f1bc7703608d
Đào Bá Đạt <daobadat7913@outlook.com>
2016-06-08 10:35:27 +0700

    Update Cart

--- application/controllers/cart.php
+++ application/controllers/cart.php
@@ -2,6 +2,7 @@
 class Cart extends Controller
 {
     private $cartModel;
+    private $listCartBook;
     function __construct()
     {
         include("application/models/carts_model.php");
@@ -10,15 +11,20 @@ class Cart extends Controller
 
     function Index()
     {
+        $_SESSION["cartBook"] = array();
+        $_SESSION["cartBook"][] = "2";
+        $_SESSION["cartBook"][] = "3";
         if(empty($_SESSION["cartBook"]))
         {
             header('Location: index.php?c=home');
             exit;
         }
 
-        
+        $this->listCartBook = $this->cartModel->GetBookByMultipleID($_SESSION["cartBook"]);
+
+        $data = array("listCartBook"=>$this->listCartBook);
         $view = array("Index" => "Index");
-        $this->View($view);
+        $this->View($view,$data);
     }
 
     function AddSessionBookID($bookID)
--- application/models/carts_model.php
+++ application/models/carts_model.php
@@ -1,7 +1,18 @@
 <?php
     class Carts extends Model
     {
+        function GetBookByMultipleID($listBookID)
+        {
+            $in = join(",",$listBookID);
 
+            //Câu Select
+            $sql = "SELECT * FROM books WHERE BOOK_ID IN($in) ";
 
+            //Thực thi câu lệnh
+            $runSql = $this->db->QueryResult($sql);
+
+            //Trả về dữ liệu
+            return $runSql->fetchAll(PDO::FETCH_CLASS);
+        }
     }
 ?>
