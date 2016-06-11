812a8b42a64da2fec821d2e7628ee23e26f9fbd7
Đào Bá Đạt <daobadat7913@outlook.com>
2016-06-11 21:54:29 +0700

    Sửa lỗi Cart

--- application/controllers/cart.php
+++ application/controllers/cart.php
@@ -1,24 +1,34 @@
 <?php
 class Cart extends Controller
 {
+    //Biến chứa carts_model
     private $cartModel;
+
+    //Biến chứa danh sách thông tin book được lưu trong giỏ hàng
     private $listCartBook;
+
     function __construct()
     {
+        //Khai báo model và khởi tạo model
         include("application/models/carts_model.php");
         $this->cartModel = new Carts();
     }
 
     function Index()
     {
+        //Kiểm tra khi vào giỏ hàng nếu giỏ hàng trống quay lại trang home
         if(empty($_SESSION["cartBook"]))
         {
             header('Location: index.php?c=home');
             exit;
         }
+        //----------------------------------------------------------------
 
+        //Nếu giỏ hàng không trống thì tạo ra 2 biến 1 biến chứa danh sách book ID
+        // và một biến chứa danh sách số lượng sách tương ứng với từng book id trong giỏ hàng
         $listBookID = array();
         $listBookQuality = array();
+
         $count = count($_SESSION["cartBook"]);
         for($i = 0; $i < $count; $i++)
         {
@@ -30,18 +40,32 @@ class Cart extends Controller
             $listBookID[] = $_SESSION["cartBook"][$i][0];
             $listBookQuality[] = $_SESSION["cartBook"][$i][1];
         }
+        //----------------------------------------------------------------------------------
 
+        //Sau khi có danh sách book ID thì truyền vào model và model sẽ trả về để thông tin đầy đủ của từng cuốn sách
         $this->listCartBook = $this->cartModel->GetBookByMultipleID($listBookID);
+        //----------------------------------------------------------------------------------------------------------
 
+        //Truyền các sách và số lượng vào biến data
         $data = array("listCartBook"=>$this->listCartBook,"listBookQuality" => $listBookQuality);
+        //-----------------------------------------
+
+
+        //Khởi tạo các view sẽ được chạy
         $view = array("Index" => "Index");
+
+        //-------------------------------
+
+        //Khởi động view và truyền data vào view đó
         $this->View($view,$data);
+        //--------------------------------
     }
 
     function AddSessionBookID()
     {
         $bookID = $_POST["id"];
-        $count= 0;
+
+
         if(empty($_SESSION["cartBook"]))
         {
             $_SESSION["cartBook"] = array();
@@ -123,8 +147,14 @@ class Cart extends Controller
 
     function CartTotal()
     {
-        $count = count($_SESSION["cartBook"]);
-        echo count($_SESSION["cartBook"]);
+        if(empty($_SESSION["cartBook"]))
+        {
+            echo 0;
+        }
+        else {
+            $count = count($_SESSION["cartBook"]);
+            echo count($_SESSION["cartBook"]);
+        }
     }
 }
 
